347f94064501d4c14491babaf557f1f5c6395dd3
Andrka <lozhnikov.andrei@gmail.com>
2020-11-17 22:36:49 +0300

    Add more tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,7 +17,7 @@ import pytest
 def test_http_api(client, sample_file, file_name, file_hash):
     """Test HTTP API."""
 
-    def test_upload(client, sample_file, file_name, file_hash):
+    def test_upload_file(client, sample_file, file_name, file_hash):
         """Test file upload."""
         global new_hash
         data = {
@@ -31,18 +31,50 @@ def test_http_api(client, sample_file, file_name, file_hash):
         assert res.status_code == 200
         assert new_hash == file_hash
 
-    def test_download(client, new_hash):
+    def test_download_file(client, new_hash):
         """Test file download."""
         url = '/download/{0}'.format(new_hash)
         res = client.get(url)
         assert res.status_code == 200
 
-    def test_delete(client, new_hash):
+    def test_delete_file(client, new_hash):
         """Test file delete."""
         url = '/delete/{0}'.format(new_hash)
         res = client.delete(url)
         assert res.status_code == 200
 
-    test_upload(client, sample_file, file_name, file_hash)
-    test_download(client, new_hash)
-    test_delete(client, new_hash)
+    test_upload_file(client, sample_file, file_name, file_hash)
+    test_download_file(client, new_hash)
+    test_delete_file(client, new_hash)
+
+
+@pytest.mark.parametrize('data', [
+    {'f': (BytesIO(b'\xE2\x82\xAC'), 'test')},
+    {'file': (BytesIO(b'\xE2\x82\xAC'), )},
+])
+def test_upload(client, data):
+    """Test upload function."""
+    res = client.post('/upload', data=data)
+    assert res.status_code == 400
+
+
+@pytest.mark.parametrize('hash, response_code', [
+    ('bca53fde466a76b7bee3e1899', 400),
+    ('bca53fde466a76b7bee3e18997e94a7a', 404),
+])
+def test_download(client, hash, response_code):
+    """Test download function."""
+    url = '/download/{0}'.format(hash)
+    res = client.get(url)
+    assert res.status_code == response_code
+
+
+@pytest.mark.parametrize('hash, response_code', [
+    ('bca53fde466a76b7bee3e1899', 400),
+    ('bca53fde466a76b7bee3e18997e94a7a', 404),
+])
+def test_delete(client, hash, response_code):
+    """Test delete function."""
+    url = '/delete/{0}'.format(hash)
+    res = client.delete(url)
+    assert res.status_code == response_code
